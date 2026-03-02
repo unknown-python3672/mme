c90f4fdc79339fce71032470b62ab9f015cf1d1e
Anthony <anthonyanyaele28@gmail.com>
2026-03-02 02:38:17 +0100

    removed vite nuxt is  better

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login-page">
+    <h1>Welcome</h1>
+    <p>Log in to purchase your own textbooks</p>
+    <form class="login-form" @submit.prevent="handleLogin">
+      <input type="text" placeholder="Username" v-model="username" required />
+      <input type="password" placeholder="Password" v-model="password" />
+      <button type="submit">Log In</button>
+      <NuxtLink to="/create-account" class="acc">Create account</NuxtLink>     
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import './assets/logincss.css'
+
+const username = ref('')
+const password = ref('')
+
+const handleLogin = () => {
+  console.log(username.value, password.value)
+}
+</script>
