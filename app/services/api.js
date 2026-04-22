42f1cdd852ea53df44ba6bf966f7487d0527a1d9
Anthony <anthonyanyaele28@gmail.com>
2026-04-23 00:58:41 +0100

    updating

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: process.env.NODE_ENV === 'production' 
+        ? 'https://mme-backend-ase4.onrender.com/api'
+        : 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json'
     }
