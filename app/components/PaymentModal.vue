cd22f6804fda4cc9053667e806a4eab08e41d792
Unknown-python <anthonyanyaele28@gmail.com>
2026-05-24 23:43:47 +0100

    #working...

--- app/components/PaymentModal.vue
+++ app/components/PaymentModal.vue
@@ -186,7 +186,6 @@ const handleSubmit = async () => {
     // ✅ Get email from cookie not localStorage
     const userCookie = useCookie('user')
     const tokenCookie = useCookie('token')
-    console.log('Token value:', tokenCookie.value) 
     if (userCookie.value) {
       form.value.email = userCookie.value.email
     }
